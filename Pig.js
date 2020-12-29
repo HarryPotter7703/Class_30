a1b3be6c50921e7be728f36b70bb5aaaf26fc7ea
HarryPotter7703 <71449543+HarryPotter7703@users.noreply.github.com>
2020-12-29 14:33:35 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,24 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.Visibility = 255
+  }
+  display(){
+
+//    console.log(this.body.speed);
+    if(this.body.speed < 3){
+      super.display();
+    }
+    else{
+      
+      World.remove(world, this.body);
+      push();
+      this.Visibility -= 5;
+      tint(255, this.Visibility);
+      image(this.image, this.body.position.x-25, this.body.position.y-25, 50, 50)
+      pop();
+    }
+  }
+
+};
